63986d51094b084eab3601eeb6140e5d3771a8d8
Deepesh Kumar <62414542+cyberdeepesh@users.noreply.github.com>
2024-10-04 14:16:59 +0530

    Update contact.js
    Add annimated Form

--- app/components/contact.js
+++ app/components/contact.js
@@ -33,48 +33,7 @@ const Contact = () => {
             </div>
           </div>
           <div>
-            <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-              <div className="mb-4">
-                <label className="block text-gray-200 text-sm font-bold mb-2" htmlFor="name">
-                  Name
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline bg-gray-700"
-                  id="name"
-                  type="text"
-                  placeholder="Your Name"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-200 text-sm font-bold mb-2" htmlFor="email">
-                  Email
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline bg-gray-700"
-                  id="email"
-                  type="email"
-                  placeholder="Your Email"
-                />
-              </div>
-              <div className="mb-6">
-                <label className="block text-gray-200 text-sm font-bold mb-2" htmlFor="message">
-                  Message
-                </label>
-                <textarea
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline h-32 bg-gray-700"
-                  id="message"
-                  placeholder="Your Message"
-                ></textarea>
-              </div>
-              <div className="flex items-center justify-center">
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                >
-                  Send Message
-                </button>
-              </div>
-            </form>
+                      <div class="visme_d" data-title="Company News Subscription" data-url="4d68337q-company-news-subscription" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="96288"></div><script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></script>
           </div>
         </div>
       </div>
